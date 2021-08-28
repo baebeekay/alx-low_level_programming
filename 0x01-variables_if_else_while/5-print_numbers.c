9c68944680a44bbc34f30962e720260d81c0b1a0
baebeekay <baebeekay6@gmail.com>
2021-08-28 07:57:53 -0700

    Base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	printf("%d", x);
+	}
+	printf("\n");
+
+	return (0);
+}
