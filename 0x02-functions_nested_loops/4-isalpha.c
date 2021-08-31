6ea6dd3ed83a34a97be1a5b1e5095382a3e19508
baebeekay <baebeekay6@gmail.com>
2021-08-31 11:06:54 -0700

    Isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	char U, l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		for (U = 'A'; U <= 'Z'; U++)
+		{
+			if (c == l || c == U)
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _putchar(char c);
+int _isalpha(int c);
 
 #endif
