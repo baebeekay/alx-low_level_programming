cfdf5e237462a1408c6d8ee49ea6710af7c83027
baebeekay <baebeekay6@gmail.com>
2021-09-09 02:47:18 -0700

    Prints integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,32 @@
  */
 void print_number(int n)
 {
+	char a;
+	int b;
+	char c;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		a = ('0' - (n % 10));
+		n /= -10;
+	} else
+	{
+		a = ((n % 10) + '0');
+		n /= 10;
 	}
-	if (n == 0)
-		_putchar(n);
+	b = 0;
+	while (n > 0)
+	{
+		b = b * 10 + (n % 10);
+		n /= 10;
+	}
 
-	if (n / 10 != 0)
-		print_number((n - n % 10) / 10);
-	_putchar(n % 10 + '0');
+	while (b > 0)
+	{
+		c = ((b % 10) + '0');
+		_putchar (c);
+		b /= 10;
+	}
+	_putchar(a);
 }
