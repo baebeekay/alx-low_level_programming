4efbe160bbefe979698651d9c807d0abe650beb4
baebeekay <baebeekay6@gmail.com>
2021-09-13 10:44:09 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# C - Even more pointers, arrays and strings
+
+0-memset.c - Write a function that fills memory with a constant byte.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
