8b3f4aa15285f12a42c831d69b66e51c185844a0
baebeekay <baebeekay6@gmail.com>
2021-09-14 06:44:36 -0700

    Set String

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-
+void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
