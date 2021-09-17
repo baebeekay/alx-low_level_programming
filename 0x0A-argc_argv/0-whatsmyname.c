7eb7fda5f53a1def8fc3c5864de33f77e44bd5b4
baebeekay <baebeekay6@gmail.com>
2021-09-17 13:28:04 -0700

    My Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+		printf("%s\n", argv[argc - argc]);
+	return (0);
+}
