e94594a8423c2438a7ce51cd74080690b1ccc1ad
baebeekay <baebeekay6@gmail.com>
2021-09-21 12:53:21 -0700

    Array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,9 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	unsigned int i = 0;
+	unsigned int i;
 
 	s = malloc(sizeof(char) * size);
-	while (i < size)
-	{
 		if (size == 0)
 		{
 			return (NULL);
@@ -23,9 +21,12 @@ char *create_array(unsigned int size, char c)
 		{
 			return (NULL);
 		}
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
+		i = 1;
+		while (i < size)
+		{
+			s[i] = c;
+			i++;
+		}
+		s[i] = '\0';
+		return (s);
 }
