ad61be7993e8ef69fbaf3fb1b1d44c62aa17a4dd
baebeekay <baebeekay6@gmail.com>
2021-09-27 09:37:37 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - entry point
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
