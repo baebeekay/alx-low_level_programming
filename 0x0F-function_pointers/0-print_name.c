24c5cd7fc2edcbc5e3679bb752a645d876ef4d6a
baebeekay <baebeekay6@gmail.com>
2021-09-29 04:05:55 -0700

    Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @f: function pointer
+ * @name: character to prints
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,29 @@
+#ifndef _FUNCTION_POINTERS_H
+#define _FINCTION_POINTERS_H
+
+
+
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
