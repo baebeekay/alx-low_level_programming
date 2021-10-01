556181081cd4c841b08dcd525f943cb1fbd8c3ee
baebeekay <baebeekay6@gmail.com>
2021-10-01 06:34:42 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints fibonacci numbbers to 50
+ * main - prints fibonacci numbers to 50
  *
  * Return: Always 0 (Success)
  */
@@ -11,7 +11,7 @@ int main(void)
 	int num2 = 2;
 	int n, x;
 
-	for (n = 0; n <= 47; n++)
+	for (n = 3; n <= 47; n++)
 	{
 		printf("%d ,", num1);
 		x = num1 + num2;
