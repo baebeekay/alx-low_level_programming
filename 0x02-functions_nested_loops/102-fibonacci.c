376984863f1a115d7d42ab1c29aafb52c1615015
baebeekay <baebeekay6@gmail.com>
2021-10-01 07:34:11 -0700

    Bugfix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	long long int num1 = 1;
-	long long int num2 = 2;
+	long int num1 = 1;
+	long int num2 = 2;
 	int i;
-	long long int x;
+	long int x;
 
-	printf("%lld, %lld, ", num1, num2);
+	printf("%lu, %lu, ", num1, num2);
 	for (i = 0; i < 48; i++)
 	{
 		x = num1 + num2;
-		printf("%lld", x);
+		printf("%lu", x);
 		num1 = num2;
 		num2 = x;
 		if (i < 47)
