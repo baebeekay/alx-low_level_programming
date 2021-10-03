8c104781e00b56eef73c13968a656e635a4eabbf
baebeekay <baebeekay6@gmail.com>
2021-10-03 08:50:44 -0700

    Bugfix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 	long int z;
 
 	printf("%lu, %lu, ", n1, n2);
-	for (y = 0; y <= 85; y++)
+	for (y = 0; y <= 82; y++)
 	{
 		z = n1 + n2;
 		printf("%lu", z);
 		n1 = n2;
 		n2 = z;
-		if (y < 85)
+		if (y < 82)
 		{
 			printf(", ");
 		}
