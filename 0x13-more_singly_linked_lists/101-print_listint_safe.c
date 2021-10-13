c310af2aad864192f3d9a09d01bb135fa8e7815a
baebeekay <baebeekay6@gmail.com>
2021-10-13 10:59:28 -0700

    Handles loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - prints elements in a list
+ * @head: pointer to elements
+ * Return: num of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		i++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head - (head->next) > 0)
+		{
+			head = head->next;
+		}
+		else
+		{
+			printf("-> [%p] %d\n",
+			       (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (i);
+}
