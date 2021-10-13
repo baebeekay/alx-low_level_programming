476a3a4e91e04b29329b76bd31f522c734739f7c
baebeekay <baebeekay6@gmail.com>
2021-10-13 06:53:33 -0700

    Add node at index position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		node->next = *head;
-		return (NULL);
+		return (node);
 	}
 	while (idx - 1 > 0)
 	{
