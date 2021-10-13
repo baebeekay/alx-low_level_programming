86e4998365b9eff77eccfff4d7f28b780746776d
baebeekay <baebeekay6@gmail.com>
2021-10-13 11:13:03 -0700

    Handles loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,11 +22,9 @@ size_t print_listint_safe(const listint_t *head)
 		}
 		else
 		{
-			printf("-> [%p] %d\n",
-			       (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
-
 	return (i);
 }
