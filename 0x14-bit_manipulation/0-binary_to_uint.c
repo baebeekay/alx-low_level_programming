d6bfddfa41288f7483f2e7a19a9e9d92fde79ed3
baebeekay <baebeekay6@gmail.com>
2021-10-15 05:00:04 -0700

    Converts binary to Integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converto binary to integers
+ * @b: binary number
+ * Return: integer converted
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int base = 1, i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		i++;
+	}
+	while (i >= 0)
+	{
+		n += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+	return (n);
+}
