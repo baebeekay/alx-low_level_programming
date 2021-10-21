3c77a91fdc5771d42cd4a8d5307eed3c60c7eb13
baebeekay <baebeekay6@gmail.com>
2021-10-21 15:17:41 -0700

    Major bugFix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,37 +14,27 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		error("Usage: cp file_from file_to");
+		printf("Usage: cp file_from file_to");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		error("Can't read from file NAME_OF_THE_FILE");
+		printf("Error: Can't read from file %s", argv[1]);
 		exit(98);
 	}
 	file_to = creat(argv[2], PERM);
 	if (file_to == -1)
 	{
-		error("Can't write to NAME_OF_THE_FILE");
+		printf("Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
 	while ((FD_VALUE = read(file_from, buf, BUFSIZE)) > 0)
 		if (write(file_to, buf, FD_VALUE) != FD_VALUE)
-			error("Can't close fd FD_VALUE");
+			printf("Error: Can't close fd FD_VALUE");
 	exit(100);
 	close(file_from);
 	close(file_to);
 
 	return (0);
-}
-/**
- * error - prints an error string
- * @s: string to print
- */
-void error(char *s)
-{
-	dprintf(STDERR_FILENO, s, "Error: ");
-	dprintf(STDERR_FILENO, "\n");
-	exit(1);
 }
