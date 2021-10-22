9b38d68ba77af6a52e593901b74fcbaa97735c1e
baebeekay <baebeekay6@gmail.com>
2021-10-22 04:02:04 -0700

    BugFix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_to, file_from, FD_VALUE;
+	int file_to, file_from, n;
 	char buf[BUFSIZE];
 
 	if (argc != 3)
@@ -34,9 +34,9 @@ int main(int argc, char *argv[])
 		printf("Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((FD_VALUE = read(file_from, buf, BUFSIZE)) > 0)
+	while ((n = read(file_from, buf, BUFSIZE)) > 0)
 	{
-		if (write(file_to, buf, FD_VALUE) != FD_VALUE)
+		if (write(file_to, buf, n) != n)
 		{
 			printf("Error\n");
 			exit(99);
@@ -44,6 +44,10 @@ int main(int argc, char *argv[])
 	}
 		close(file_to);
 		close(file_from);
-
-	return (0);
+		if ((close(file_to) || close(file_from)) != 0)
+		{
+			printf("Error: Can't close fd -1\n");
+			exit(100);
+		}
+		return (0);
 }
