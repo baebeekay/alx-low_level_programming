8b37aa3a15c79bf174d4411988758f6269365479
baebeekay <baebeekay6@gmail.com>
2021-10-22 18:22:21 -0700

    Elf file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-	unsigned char e_ident[EI_NIDENT];
-	uint16_t      e_type;
-	uint16_t      e_machine;
-	uint32_t      e_version;
-	ElfN_Addr     e_entry;
-} ElfN_Ehdr;
 
 /**
  * main - entry point
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	printf("Error: not ELF file\n");
-	exit(98);
-
 	return (0);
 }
