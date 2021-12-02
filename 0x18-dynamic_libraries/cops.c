3f23f002467d691060e3410570b5782d2b0c9c36
baebeekay <baebeekay6.com>
2021-12-02 12:59:53 -0800

    Calling C library fron python

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - gets the sum
+ * @a: first int
+ * @b: second int
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - gets the difference
+ * @a: first int
+ * @b: second int
+ * Return: the difference
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - gets the product
+ * @a: first int
+ * @b: second int
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two int
+ * @a: first int
+ * @b: second int
+ * Return: the result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - gets the modulus
+ * @a: first int
+ * @b: second int
+ * Return: the remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <stdlib.h>
+#include <stdio.h>
 
 
 int _putchar(char c);
@@ -24,6 +24,13 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+
 
 
 #endif
